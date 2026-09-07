58191eb2e2581e080d98ed5faddf86cc4d379e44
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2026-09-07 10:56:59 +0300

    Исправить XML-документацию RussianPluralExtensions

--- Archivist/Localization/RussianPluralExtensions.cs
+++ Archivist/Localization/RussianPluralExtensions.cs
@@ -1,20 +1,22 @@
 ﻿namespace dRz.GPT_Utilities.Archivist.Localization
 {
-    /// <summary>
-    /// русский язык Pluralization Extensions
-    /// </summary>
+    /// <summary>Методы склонения русских существительных по числу.</summary>
     public static class RussianPluralExtensions
     {
+        /// <summary>
+        /// Возвращает соответствующую числу форму существительного.
+        /// </summary>
+        /// <param name="number">Число, для которого выбирается форма.</param>
+        /// <param name="forms">Набор форм существительного.</param>
+        /// <param name="onlyWord">Если <see langword="true"/>, возвращается только слово без числа.</param>
+        /// <returns>Строка с числом и существительным либо только существительное.</returns>
         public static string Of(this int number, RussianPluralForms forms, bool onlyWord = false)
         {
             int value = Math.Abs(number);
-
             string word;
 
             if (value % 100 is >= 11 and <= 19)
-            {
                 word = forms.Many;
-            }
             else
             {
                 word = (value % 10) switch
@@ -24,11 +26,8 @@
                     _ => forms.Many
                 };
             }
-            if (onlyWord)
-            {
-                return $"{word}";
-            }
-            return $"{number} {word}";
+
+            return onlyWord ? word : $"{number} {word}";
         }
     }
 }
