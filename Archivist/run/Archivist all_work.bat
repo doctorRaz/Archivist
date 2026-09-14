5f1c50da7042f299afa232003abc9cdf0b256d56
doctorRAZ_h <doctorRAZ@yandex.ru>
2026-09-14 22:07:23 +0300

    work bat

---
+++ Archivist/run/Archivist all_work.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 >nul
+setlocal
+
+"d:\@Developers\Programmers\!NET\!Archivist\bin\Release\Archivist.exe" -s "c:\Users\razygraevaa\Downloads" -d "d:\@Developers\В работе\Reminder\GPT-export\Markdown" -p "*-export-markdown*" -a
+
+
+REM pause
